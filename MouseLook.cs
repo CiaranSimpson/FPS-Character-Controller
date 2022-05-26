f8922d3bd6b6c0bcc0dadc7a2909025beff7fe58
CiaranSimpson <43167249+CiaranSimpson@users.noreply.github.com>
2022-05-26 11:49:14 +1000

    Rewrote code and made clamping actually work

--- MouseLook.cs
+++ MouseLook.cs
@@ -5,56 +5,52 @@ using UnityEngine.InputSystem;
 
 public class MouseLook : MonoBehaviour
 {
-    Vector2 mousePos;
+    float inputY, inputX, rotY, rotX;
     public Camera cam;
 
-    public float upClamp, downClamp;
+    [Range (0,90)] public float maxX, minX;
 
-    [Range(1,10)] public float sensitivity = 2;
+    [Range(1, 10)] public float Xsensitivity = 2;
+    [Range(1, 10)] public float Ysensitivity = 2;
 
-    public void GetMousePos (InputAction.CallbackContext context)
+    public CursorLockMode cursorMode = CursorLockMode.Locked;
+
+    public void GetMousePos(InputAction.CallbackContext context)
     {
-        mousePos = ((context.ReadValue<Vector2>() / 2) * sensitivity);
+        inputY = ((context.ReadValue<Vector2>().x / 2) * Ysensitivity);
+        inputX = ((context.ReadValue<Vector2>().y / 2) * Xsensitivity);
         //print(mousePos);
     }
 
 
     void Update()
     {
+        GetValues();
         UpdateCamera();
+        ManageCursor();
+
+    }
+
+    void GetValues()
+    {
+        rotY = inputY;
+        rotX += -inputX;
+        rotX = Mathf.Clamp(rotX, minX, maxX);
     }
 
     void UpdateCamera()
     {
-        UpdateX();
-        UpdateY();
+        transform.Rotate(new Vector3(0, rotY, 0), Space.World);
+
+        cam.transform.localRotation = Quaternion.Euler(rotX, cam.transform.rotation.y, 0);
     }
 
-    void UpdateX()
+    void ManageCursor()
     {
-        float desiredRotation = -mousePos.y;
-        float finalRotation = desiredRotation;
+        Cursor.lockState = cursorMode;
 
-        bool clamp = CiaransLibrary.Clamp(cam.transform.rotation.x, upClamp, -downClamp);
-        //print(clamp);
-        if (!clamp)
-        {
-            cam.transform.Rotate(new Vector3(finalRotation, 0, 0), Space.Self);
-        }
-        else
-        {
-            
-        }
-
-        
-        //Debug.Log(desiredRotation.ToString() + " " + finalRotation.ToString());
     }
 
-    void UpdateY()
-    {
-        transform.Rotate(new Vector3(0, mousePos.x, 0), Space.World);
-    }
 
-    
 
 }
